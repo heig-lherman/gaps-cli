5546b0e9bfaf639202336ad68df7b4ec6e88efcc
Loïc Herman <loic@loicherman.ch>
2024-02-19 20:07:52 +0100

    feat: use system keyring to save password and tokens

--- cmd/login.go
+++ cmd/login.go
@@ -2,10 +2,12 @@ package cmd
 
 import (
 	"bufio"
+	"errors"
 	"fmt"
 	log "github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
+	"github.com/zalando/go-keyring"
 	"golang.org/x/term"
 	"lutonite.dev/gaps-cli/gaps"
 	"lutonite.dev/gaps-cli/util"
@@ -14,9 +16,9 @@ import (
 )
 
 const (
+	ServiceKey        = "gaps-cli"
+	KeyringTokenKey   = "gaps.token"
 	UsernameViperKey  = "login.username"
-	PasswordViperKey  = "login.password"
-	TokenValueKey     = "login.token.value"
 	TokenStudentIdKey = "login.token.studentId"
 	TokenDateValueKey = "login.token.generatedAt"
 )
@@ -31,7 +33,7 @@ var (
 		Use:   "login",
 		Short: "Allows to login to GAPS for future commands",
 		Run: func(cmd *cobra.Command, args []string) {
-			if viper.GetString(TokenValueKey) != "" {
+			if token, _ := getKeyringValue(KeyringTokenKey); token != "" && !loginOpts.changePassword {
 				// Default session duration is 6 hours on GAPS
 				if !isTokenExpired() {
 					log.Info("User already logged in, keeping existing token")
@@ -54,14 +56,16 @@ var (
 				username = viper.GetString(UsernameViperKey)
 			}
 
-			if viper.GetString(PasswordViperKey) == "" || loginOpts.changePassword {
+			if pwd, _ := getKeyringValue(username); pwd == "" || loginOpts.changePassword {
 				fmt.Print("Enter your HEIG-VD einet AAI password: ")
 				passwordBytes, err := term.ReadPassword(int(os.Stdin.Fd()))
 				password = string(passwordBytes)
 				fmt.Println("ok")
 				util.CheckErr(err)
 			} else {
-				password = viper.GetString(PasswordViperKey)
+				var err error
+				password, err = getKeyringValue(username)
+				util.CheckErr(err)
 			}
 
 			refreshToken(username, password)
@@ -75,8 +79,6 @@ func init() {
 	loginCmd.Flags().BoolVar(&loginOpts.changePassword, "clear-password", false, "reset the password stored in the config file (if any)")
 
 	viper.BindPFlag(UsernameViperKey, loginCmd.Flags().Lookup("username"))
-	viper.BindPFlag(PasswordViperKey, loginCmd.Flags().Lookup("password"))
-	viper.SetDefault(TokenValueKey, "")
 	viper.SetDefault(TokenStudentIdKey, -1)
 	viper.SetDefault(TokenDateValueKey, time.Now().UnixMilli())
 
@@ -89,7 +91,8 @@ func isTokenExpired() bool {
 
 func refreshToken(username string, password string) {
 	viper.Set(UsernameViperKey, username)
-	viper.Set(PasswordViperKey, password)
+	err := keyring.Set(ServiceKey, username, password)
+	util.CheckErr(err)
 
 	cfg := new(gaps.ClientConfiguration)
 	cfg.Init(viper.GetString(UrlViperKey))
@@ -107,7 +110,9 @@ func refreshToken(username string, password string) {
 	log.Tracef("Token: %s", token)
 	log.Tracef("Student Id: %d", studentId)
 
-	viper.Set(TokenValueKey, token)
+	err = keyring.Set(ServiceKey, KeyringTokenKey, token)
+	util.CheckErr(err)
+
 	viper.Set(TokenStudentIdKey, studentId)
 	viper.Set(TokenDateValueKey, time.Now().UnixMilli())
 
@@ -116,18 +121,35 @@ func refreshToken(username string, password string) {
 }
 
 func buildTokenClientConfiguration() *gaps.TokenClientConfiguration {
-	if viper.GetString(TokenValueKey) == "" {
+	token, _ := getKeyringValue(KeyringTokenKey)
+	if token == "" {
 		log.Fatal("No token found, please login first")
 	}
 
 	// if token is expired, refresh it
 	if isTokenExpired() {
 		log.Info("Token expired, attempting refresh")
-		refreshToken(viper.GetString(UsernameViperKey), viper.GetString(PasswordViperKey))
+
+		pwd, err := keyring.Get(ServiceKey, viper.GetString(UsernameViperKey))
+		util.CheckErr(err)
+
+		refreshToken(viper.GetString(UsernameViperKey), pwd)
 	}
 
 	cfg := new(gaps.TokenClientConfiguration)
-	cfg.InitToken(viper.GetString(UrlViperKey), viper.GetString(TokenValueKey), viper.GetUint(TokenStudentIdKey))
+	cfg.InitToken(viper.GetString(UrlViperKey), token, viper.GetUint(TokenStudentIdKey))
 
 	return cfg
+}
+
+func getKeyringValue(key string) (string, error) {
+	secret, err := keyring.Get(ServiceKey, key)
+
+	if errors.Is(err, keyring.ErrNotFound) {
+		return "", nil
+	} else if err != nil {
+		return "", err
+	}
+
+	return secret, nil
 }
--- go.mod
+++ go.mod
@@ -9,12 +9,16 @@ require (
 	github.com/sirupsen/logrus v1.9.0
 	github.com/spf13/cobra v1.6.1
 	github.com/spf13/viper v1.15.0
+	github.com/zalando/go-keyring v0.2.3
 	golang.org/x/term v0.5.0
 )
 
 require (
+	github.com/alessio/shellescape v1.4.1 // indirect
 	github.com/andybalholm/cascadia v1.3.1 // indirect
+	github.com/danieljoos/wincred v1.2.0 // indirect
 	github.com/fsnotify/fsnotify v1.6.0 // indirect
+	github.com/godbus/dbus/v5 v5.1.0 // indirect
 	github.com/hashicorp/hcl v1.0.0 // indirect
 	github.com/inconshreveable/mousetrap v1.0.1 // indirect
 	github.com/magiconair/properties v1.8.7 // indirect
@@ -28,7 +32,7 @@ require (
 	github.com/spf13/pflag v1.0.5 // indirect
 	github.com/subosito/gotenv v1.4.2 // indirect
 	golang.org/x/net v0.7.0 // indirect
-	golang.org/x/sys v0.5.0 // indirect
+	golang.org/x/sys v0.8.0 // indirect
 	golang.org/x/text v0.7.0 // indirect
 	gopkg.in/ini.v1 v1.67.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
